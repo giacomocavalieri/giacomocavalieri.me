d65a49a3710acc8f39d29b3759f6f881906b44c8
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2025-08-15 11:03:24 +0200

    format ffi

--- src/markdown.ffi.mjs
+++ src/markdown.ffi.mjs
@@ -3,60 +3,40 @@ import { fromMarkdown } from "mdast-util-from-markdown";
 import * as Markdown from "./markdown/ffi_builders.mjs";
 
 const empty = new Empty();
-const fold_into_list = (arr, f) =>
-  arr.reduceRight((acc, val) => new NonEmpty(f(val), acc), empty);
+const fold_into_list = (arr, f) => arr.reduceRight((acc, val) => new NonEmpty(f(val), acc), empty);
 
 export function parse(markdown) {
   const ast = fromMarkdown(markdown);
-  const content = fold_into_list(
-    ast.children,
-    function to_lustre_element(node) {
-      switch (node.type) {
-        case "code":
-          return Markdown.code(node.value, node.lang);
-        case "emphasis":
-          return Markdown.emphasis(
-            fold_into_list(node.children, to_lustre_element),
-          );
-        case "heading":
-          return Markdown.linked_heading(node.depth, node.children[0].value);
-        case "inlineCode":
-          return Markdown.inline_code(node.value);
-        case "link":
-          return Markdown.link(
-            node.url,
-            fold_into_list(node.children, to_lustre_element),
-          );
-        case "list":
-          return Markdown.list(
-            !!node.ordered,
-            fold_into_list(node.children, to_lustre_element),
-          );
-        case "listItem":
-          return Markdown.list_item(
-            fold_into_list(node.children, to_lustre_element),
-          );
-        case "paragraph":
-          return Markdown.paragraph(
-            fold_into_list(node.children, to_lustre_element),
-          );
-        case "strong":
-          return Markdown.strong(
-            fold_into_list(node.children, to_lustre_element),
-          );
-        case "text":
-          return Markdown.text(node.value);
-        case "blockquote":
-          return Markdown.blockquote(
-            fold_into_list(node.children, to_lustre_element),
-          );
-        case "thematicBreak":
-          return Markdown.thematic_break();
-        default:
-          console.log("unhandled:", node);
-          return Markdown.error();
-      }
-    },
-  );
+  const content = fold_into_list(ast.children, function to_lustre_element(node) {
+    switch (node.type) {
+      case "code":
+        return Markdown.code(node.value, node.lang);
+      case "emphasis":
+        return Markdown.emphasis(fold_into_list(node.children, to_lustre_element));
+      case "heading":
+        return Markdown.linked_heading(node.depth, node.children[0].value);
+      case "inlineCode":
+        return Markdown.inline_code(node.value);
+      case "link":
+        return Markdown.link(node.url, fold_into_list(node.children, to_lustre_element));
+      case "list":
+        return Markdown.list(!!node.ordered, fold_into_list(node.children, to_lustre_element));
+      case "listItem":
+        return Markdown.list_item(fold_into_list(node.children, to_lustre_element));
+      case "paragraph":
+        return Markdown.paragraph(fold_into_list(node.children, to_lustre_element));
+      case "strong":
+        return Markdown.strong(fold_into_list(node.children, to_lustre_element));
+      case "text":
+        return Markdown.text(node.value);
+      case "blockquote":
+        return Markdown.blockquote(fold_into_list(node.children, to_lustre_element));
+      case "thematicBreak":
+        return Markdown.thematic_break();
+      default:
+        console.log("unhandled:", node);
+        return Markdown.error();
+    }
+  });
   return content;
 }
