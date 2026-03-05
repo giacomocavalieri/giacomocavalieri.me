6396737fdf0f04f2fdb5f60ce5cf6ed10d5da2dd
giacomocavalieri <giacomo.cavalieri@icloud.com>
2026-03-05 21:06:27 +0000

    wip

---
+++ js/birdie_terminal-ym7aeztf.js
@@ -0,0 +1,2 @@
+function n(e){let t=document.getElementById(e);if(t!==null)t.scrollIntoView({behavior:"smooth",block:"center"});return}function o(e){let t=document.getElementById(e);if(t!==null)t.focus({preventScroll:!0});return}function r(e,t){return setTimeout(()=>t(),e)}function u(e){clearTimeout(e);return}
+export{n as a,o as b,r as c,u as d};
